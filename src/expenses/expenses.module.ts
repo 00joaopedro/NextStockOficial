e818ce887d2fd4e6ed11854a5cb14e1fbe7d3ea7
joao pedro-ch <joaop8988@github.com>
2026-06-12 18:52:58 -0300

    update despesas.html 2

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { StorageModule } from '../storage/storage.module';
+import { SystemModule } from '../system/system.module';
 import { TenancyModule } from '../tenancy/tenancy.module';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesService } from './expenses.service';
 
 @Module({
-  imports: [PrismaModule, TenancyModule, StorageModule],
+  imports: [PrismaModule, TenancyModule, StorageModule, SystemModule],
   controllers: [ExpensesController],
   providers: [ExpensesService],
   exports: [ExpensesService],
